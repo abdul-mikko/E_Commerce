e07784afd90cc3fce91b6d5bb40a6deb55057253
Adil <abdul.adil.cs@gmail.com>
2020-04-29 11:23:56 -0700

    Added Homepage and Styles

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Homepage from './homepage.comp';
 
 function App() {
   return (
     <div>
       <h1>Bismillah</h1>
+      <Homepage />
     </div>
   );
 }
