3be4ff81ddae8888791bda892e9586b5a7be80fd
Adil <abdul.adil.cs@gmail.com>
2020-04-29 15:26:48 -0700

    Added Routes to shop collection

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Homepage from './Pages/homepage.comp';
+import { Route } from 'react-router-dom';
 
-
+function HatsPage() {
+  return (
+    <h1>HATS PAGE</h1>
+  )
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={Homepage}></Route>
+      <Route path="/shop/hats" component={HatsPage}></Route>
     </div>
   );
 }
--- crwn-clothing/src/Components/directory/directory.comp.jsx
+++ crwn-clothing/src/Components/directory/directory.comp.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MenuItem from "../Menu-items/Menu-items.comp";
 
 import "./directory.styles.scss";
+
 class Directory extends React.Component {
   constructor() {
     super();
@@ -48,8 +49,8 @@ class Directory extends React.Component {
     return (
       <div>
         <div className="directory-menu">
-          {this.state.sections.map(({ title, imageUrl, size, id }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          {this.state.sections.map(({ id, ...OtherProps }) => (
+            <MenuItem key={id} {...OtherProps} />
           ))}
         </div>
       </div>
