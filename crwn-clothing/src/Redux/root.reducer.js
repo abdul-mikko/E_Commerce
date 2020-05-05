b9fd952f4dd29e21198b7b01842a5e21309531cc
Adil <abdul.adil.cs@gmail.com>
2020-05-05 13:15:18 -0700

    moved directory state to store'

--- crwn-clothing/src/Redux/root.reducer.js
+++ crwn-clothing/src/Redux/root.reducer.js
@@ -1,10 +1,22 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './User/User.reducer'
 import CartReducer from './Cart/Cart_Reducer'
+import directoryReducer from './HomePageDirectory/Directory.Reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: CartReducer
+    cart: CartReducer,
+    directory: directoryReducer
 })
 
+export default persistReducer(persistConfig, rootReducer)
